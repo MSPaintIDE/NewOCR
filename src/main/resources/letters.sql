9d8ab1ad6f34ef638e57ddb1aac02e6e1578a199
RubbaBoy <adamyarris@yahoo.com>
2018-10-02 15:39:45 -0400

    Fixed bug with decimals and doubles

--- src/main/resources/letters.sql
+++ src/main/resources/letters.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `letters` (
   letter INTEGER PRIMARY KEY UNIQUE, -- The letter the data set is for
-  avgWidth DECIMAL, -- The average width of all tested character images of this letter
-  avgHeight DECIMAL, -- The average height of all tested character images of this letter
+  avgWidth DOUBLE, -- The average width of all tested character images of this letter
+  avgHeight DOUBLE, -- The average height of all tested character images of this letter
   minFontSize INTEGER, -- The minimum font size this data set was trained on
   maxFontSize INTEGER -- The maximum font size this data set was trained on
 );
--- src/main/resources/sectionData.sql
+++ src/main/resources/sectionData.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `sectionData` (
   letter INTEGER,
   sectionIndex INTEGER,
-  data DECIMAL
+  data DOUBLE
 );
