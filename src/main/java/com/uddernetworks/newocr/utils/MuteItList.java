a8af7e0ad05acd30df978d58a28a15663270d2b0
RubbaBoy <adamyarris@yahoo.com>
2019-03-16 00:48:33 -0400

    Finished docs for MuteItList, removed some unnecessary pieces of code; ready to be used

--- src/main/java/com/uddernetworks/newocr/utils/MuteItList.java
+++ src/main/java/com/uddernetworks/newocr/utils/MuteItList.java
@@ -2,7 +2,6 @@ package com.uddernetworks.newocr.utils;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.function.Consumer;
 
@@ -16,34 +15,8 @@ import java.util.function.Consumer;
  */
 public class MuteItList<E> extends ArrayList<E> {
 
-    // TODO: Clean and add tests
-    public static void main(String[] args) throws Exception {
-        System.out.println("Starting");
-        var test = new MuteItList<String>();
-        for (int i = 0; i < 7; i++) {
-            test.add("" + i);
-        }
-//        test.add("one");
-//        test.add("two");
-//        test.add("three");
-//        test.add("four");
-//        test.add("five");
-        System.out.println("Made");
-
-        test.forEach(element -> {
-            System.out.println("element = " + element);
-
-            if (element.equals("3")) {
-                test.add(0, "7");
-            }
-        });
-
-        System.out.println("test = " + test);
-    }
-
     private Object[] elementData = null;
 
-    private boolean copyInternalElements = true;
     private int sizeMod = 0;
     private int activelyLooping = -1;
     private boolean modified = false;
@@ -59,9 +32,11 @@ public class MuteItList<E> extends ArrayList<E> {
         }
     }
 
+    /**
+     * Updates the current class' elementData field from its super elementData field via cached reflection. This is a
+     * heavy operation (In comparison) as all reflection, so it should be used sparingly.
+     */
     private void refreshElementData() {
-        if (!this.copyInternalElements) return;
-
         try {
             elementData = (Object[]) reflected.get(this);
         } catch (IllegalAccessException e) {
@@ -69,12 +44,19 @@ public class MuteItList<E> extends ArrayList<E> {
         }
     }
 
+    /**
+     * Iterated over the elements in the current list the same as {@link ArrayList#forEach(Consumer)}, however in the
+     * Consumer, you can invoke the methods {@link MuteItList#add(Object)}, {@link MuteItList#add(int, Object)},
+     * {@link MuteItList#remove(int)}, and {@link MuteItList#remove(Object)} upon the current list. Unlike a normal
+     * {@link ArrayList}, this will not cause any {@link java.util.ConcurrentModificationException}; if anything bad
+     * happens it will just break, but in testing has appeared to be fairly stable.
+     *
+     * @param action The consumer to take each element of the current list
+     */
     @Override
     public void forEach(Consumer<? super E> action) {
         sizeMod = 0;
         refreshElementData();
-        copyInternalElements = true;
-        System.out.println("elementData = " + Arrays.toString(elementData));
         modified = false;
 
         Objects.requireNonNull(action);
@@ -96,32 +78,13 @@ public class MuteItList<E> extends ArrayList<E> {
                 modified = false;
             }
         }
-//        if (modCount != expectedModCount)
-//            throw new ConcurrentModificationException();
 
         sizeMod = 0;
         activelyLooping = -1;
-        copyInternalElements = false;
     }
 
     private static <E> E elementAt(Object[] es, int index) {
         return (E) es[index];
-    }
-
-    public boolean isModified() {
-        return modified;
-    }
-
-    public void setModified(boolean modified) {
-        this.modified = modified;
-    }
-
-    public boolean isCopyInternalElements() {
-        return copyInternalElements;
-    }
-
-    public void setCopyInternalElements(boolean copyInternalElements) {
-        this.copyInternalElements = copyInternalElements;
     }
 
     @Override
